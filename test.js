59b03e6e6282f7d858d7dc866ab78e2cff8b4dad
slashhuang <627284482@qq.com>
2017-03-21 00:01:43 +0800

    finish promsie co repo

---
+++ test.js
@@ -0,0 +1,30 @@
+
+
+/*测试co*/
+
+let co = require('./co');
+
+var p1 = (val)=>Promise.resolve({
+	then:(resolve,reject)=>{
+		setTimeout(resolve,1000,val);
+	}
+})
+
+function* s(){
+	let y = yield p1('a');
+	console.log(y);
+	let q = yield p1('b');
+	console.log(q);
+	return y + q
+};
+let gen = s();
+
+// print 
+// a
+// b
+// finally
+// ab
+co(gen).then((t)=>{
+	console.log('finally');
+	console.log(t)
+})
